f942d9617aea21319c53359a48bcd0cff7221dd2
aa <linhlinhbehy@gmail.com>
2024-09-29 01:42:41 +0700

    phien ban 1

--- src/main/java/gui/FrmQLSinhVien.java
+++ src/main/java/gui/FrmQLSinhVien.java
@@ -86,25 +86,108 @@ public class FrmQLSinhVien extends JFrame {
     }
 
     private void createEvent() {
-        btDocFile.addActionListener((e)->{
+        btDocFile.addActionListener((e) -> {
             qlsv.DocDanhSachSinhVien(FILE_NAME);
-            loadData();
+            loadDataToJTable();
         });
-        btThem.addActionListener((e)->{
-            String error="";
-            String ma=txtMaSo.getText();
-            String ten=txtHoTen.getText();
-            
-                    
+        btThem.addActionListener((e) -> {
+            String error = "";
+            if (txtMaSo.getText().length() == 0) {
+                error = "Chưa nhập mã sinh viên";
+            } else {
+                String maso = txtMaSo.getText();
+                for (char c : maso.toCharArray()) {
+                    if (!Character.isDigit(c)) {
+                        error += "Mã sinh viên không được chứa chữ";
+                        break;
+                    }
+                }
+            }
+            if (txtHoTen.getText().length() == 0) {
+                error += "\nChưa nhập họ tên sinh viên";
+            } else {
+                String hoten = txtHoTen.getText();
+                for (char c : hoten.toCharArray()) {
+                    if (Character.isDigit(c)) {
+                        error += "Họ tên sinh viên không được chứa số\n";
+                        break;
+                    }
+                }
+            }
+            if (txtDTB.getText().length() == 0) {
+                error += "\n Chưa nhập diểm trung bình";
+            } else {
+                String maso = txtDTB.getText();
+                for (char c : maso.toCharArray()) {
+                    if (!Character.isDigit(c)) {
+                        error += "Điểm trung bình không được chứa chữ";
+                        break;
+                    }
+                }
+            }
+            if (error.length() > 0) {
+                JOptionPane.showMessageDialog(this, error, "Lỗi nhập liệu", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            String maso = txtMaSo.getText();
+            String hoten = txtHoTen.getText();
+            boolean gioitinh = rdNam.isSelected();
+            double diemtb = Double.parseDouble(txtDTB.getText());
+
+            SinhVien sv = new SinhVien(maso, hoten, gioitinh, diemtb);
+
+            if (qlsv.themSV(sv)) {
+
+                loadDataToJTable();
+                JOptionPane.showMessageDialog(this, "Đã thêm sinh viên thành công", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(this, "Thao tác thêm thất bại\nDo trùng mã sinh viên", "Thông báo", JOptionPane.ERROR_MESSAGE);
+            }
+
+        });
+        btXoa.addActionListener((e) -> {
+            String maso = "";
+            int selectedRowIndex = tblSinhVien.getSelectedRow();
+            if (selectedRowIndex < 0) {
+                JOptionPane.showMessageDialog(this, "Bạn chưa chọn sinh viên cần xoá", "Thông báo", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            maso = tblSinhVien.getValueAt(selectedRowIndex, 0).toString();
+            int confirm = JOptionPane.showConfirmDialog(this, "Bạn có chắc chắn muốn xoá sinh viên với mã số: " + maso + "?", "Xác nhận xoá", JOptionPane.YES_NO_OPTION);
+            if (confirm == JOptionPane.YES_OPTION) {
+                if (qlsv.xoaSV(maso)) {
+                    loadDataToJTable();
+                    JOptionPane.showMessageDialog(this, "Đã xoá sinh viên thành công", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(this, "Thao tác xoá thất bại\nDo tồn tại", "Thông báo", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        btGhiFile.addActionListener((e) -> {
+
+            if (qlsv.GhiDanhSachSinhVien(FILE_NAME)) {
+                JOptionPane.showMessageDialog(this, "Đã ghi dữ liệu thành công", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(this, "Đã ghi dữ liệu thất bại", "Thông báo", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        chkSapXep.addItemListener((e) -> {
+
+            if (chkSapXep.isSelected()) {
+                qlsv.sapXepTheoHocLuc();
+                loadDataToJTable();
+            }
+
         });
     }
 
-    private void loadData() {
+    private void loadDataToJTable() {
         model.setRowCount(0);
-        for(SinhVien sv:qlsv.getDsSinhVien()){
-            model.addRow(new Object[]{sv.getMaso(),sv.getHoten(),sv.isGioitinh(),sv.getDiemTB(),sv.getHocLuc()});
+        for (SinhVien sv : qlsv.getDsSinhVien()) {
+            model.addRow(new Object[]{sv.getMaso(), sv.getHoten(), sv.isGioitinh(), sv.getDiemTB(), sv.getHocLuc()});
         }
     }
 
 }
-
--- src/main/java/model/QLSinhVien.java
+++ src/main/java/model/QLSinhVien.java
@@ -5,6 +5,8 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import util.FileHelper;
 
 /**
@@ -12,16 +14,17 @@ import util.FileHelper;
  * @author ADMIN
  */
 public class QLSinhVien {
+
     private ArrayList<SinhVien> dsSinhVien;
-    
+
     public QLSinhVien() {
-        dsSinhVien = new ArrayList<>();        
+        dsSinhVien = new ArrayList<>();
     }
 
     public QLSinhVien(ArrayList<SinhVien> ds) {
         this.dsSinhVien = ds;
     }
-    
+
     public ArrayList<SinhVien> getDsSinhVien() {
         return dsSinhVien;
     }
@@ -29,14 +32,13 @@ public class QLSinhVien {
     public void setDsSinhVien(ArrayList<SinhVien> dsSinhVien) {
         this.dsSinhVien = dsSinhVien;
     }
-    
-    public void DocDanhSachSinhVien(String filename)
-    {
-        ArrayList<String> data= FileHelper.readFileText(filename);
+
+    public void DocDanhSachSinhVien(String filename) {
+        ArrayList<String> data = FileHelper.readFileText(filename);
         dsSinhVien.clear();
-        for(String item:data){
-            String [] arr=item.split(";");
-            SinhVien sv=new SinhVien();
+        for (String item : data) {
+            String[] arr = item.split(";");
+            SinhVien sv = new SinhVien();
             sv.setMaso(arr[0]);
             sv.setHoten(arr[1]);
             sv.setGioitinh(Boolean.parseBoolean(arr[2]));
@@ -45,27 +47,55 @@ public class QLSinhVien {
         }
     }
 
-    public boolean GhiDanhSachSinhVien(String filename)
-    {      
-        
+    public boolean GhiDanhSachSinhVien(String filename) {
+
+        ArrayList<String> data = new ArrayList<>();
+        for (SinhVien sv : dsSinhVien) {
+            String info = sv.getMaso() + ";" + sv.getHoten() + ";" + sv.isGioitinh() + ";" + sv.getDiemTB();
+            data.add(info);
+        }
+        return FileHelper.writeFileText(filename, data);
+    }
+
+    public boolean themSV(SinhVien sv) {
+        for (SinhVien item : dsSinhVien) {
+            if (item.getMaso().equals(sv.getMaso())) {
+                return false;
+            }
+        }
+        dsSinhVien.add(sv);
+        // phuong thuc equals
+//       if (dsSinhVien.contains(sv)) {
+//            return false;
+//        }
+//
+//        dsSinhVien.add(sv);
+//        return true;
         return true;
-    }    
-    
-    public boolean themSV(SinhVien sv)
-    {
-       //sinh viên viết code 
-        return true;
-    }  
-    
-    public boolean xoaSV(String maso)
-    {
-        //sinh viên viết code 
-        return true;
-    }     
-   
-    public void sapXepTheoHocLuc()
-    {
-        //sinh viên viết code         
-    }   
-    
+    }
+
+    public boolean xoaSV(String maso) {
+
+        for (int i = 0; i < dsSinhVien.size(); i++) {
+            if (dsSinhVien.get(i).getMaso().equals(maso)) {
+                dsSinhVien.remove(i);
+                return true;
+            }
+
+        }
+//        SinhVien sv = new SinhVien(maso);
+//        if (dsSinhVien.contains(sv)) {
+//            dsSinhVien.remove(sv);
+//            return true;
+//        }
+        return false;
+    }
+
+    public void sapXepTheoHocLuc() {
+        Comparator<SinhVien> cmp = (sv1, sv2) -> {
+            return Double.compare(sv1.getDiemTB(), sv2.getDiemTB());
+        };
+        Collections.sort(dsSinhVien, cmp);
+    }
+
 }
